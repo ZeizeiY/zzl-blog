6b6dea5a61c135b86e529b5bcc0c216486fead86
ZeizeiY <935604045@qq.com>
2026-05-27 23:26:11 +0800

    config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-
+output: "export",  // 启用静态导出
+  trailingSlash: true,  // 可选，确保路由正确
+  images: {
+    unoptimized: true,  // 禁用 Next.js 图片优化（静态导出必需）
+  },
 };
 
 export default nextConfig;
